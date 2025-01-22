071b7ac8862448a5db4ea43aab5f8f5511a1b512
MiguelFS <lflores.221ds15@istta.edu.pe>
2025-01-21 21:34:03 -0500

    version 0.2

--- app.js
+++ app.js
@@ -8,7 +8,46 @@ function agregarAmigo(){
         alert("Por favor, inserte un nombre.");
     }else{
         amigos.push(nombre.value);
-        document.querySelector("#amigo").value="";
-        console.log(amigos);
+        listarAmigos();
+        console.log(amigos)
     }
-}s
+    return;
+}
+
+function listarAmigos(){
+    //Seleccionar el elemento ul
+    let lista=document.querySelector("#listaAmigos");
+    //Limpiar si hay algun contenido
+    limpiarCampos(lista);
+    for (let i = 0; i < amigos.length; i++) {
+        //crear un nuevo elemento <li>
+        let item=document.createElement("li")
+        //Establecer el contenido al <li>
+        item.textContent=amigos[i];
+        console.log(amigos[i]);
+        //Agregar el <li> a la lista
+        lista.appendChild(item);
+    }
+    return;
+}
+
+function limpiarCampos(lista){
+    document.querySelector("#amigo").value="";
+    lista.innerHTML="";
+    return;
+}
+
+function sortearAmigo(){
+    if(!amigos.length==0){
+      let numeroS=numRandom();
+      document.getElementById("resultado").innerHTML=`El amigo secreto es: ${amigos[numeroS]}`;
+      console.log(numeroS);  
+    }else{
+        alert("Ingrese nombre de sus amigos")
+    }
+    
+}
+
+function numRandom(){
+    return Math.floor(Math.random()*amigos.length);
+}
